acb34b576e63dca8bf763b377f35cee7c1c4955a
Sanyam243 <sanyamjain2240@gmail.com>
2025-01-14 23:55:40 +0530

    Workspace-added

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,27 +1,46 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { MessageContext } from './context/MessageContext'
 import { UserContext } from './context/UserContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-function Provider({children}) {
-  const [messages,setMessages] =useState();
-  const [user,setUser] =useState();
+import { useConvex } from 'convex/react';
+import { api } from '../convex/_generated/api';
+function Provider({ children }) {
+  const convex = useConvex();
+  const [messages, setMessages] = useState();
+  const [user, setUser] = useState();
+  const isAuthenticated = async () => {
+    if (typeof window != undefined) {
+      const user = JSON.parse(localStorage.getItem('user'))
+      //Fetch the user
+      const result = await convex.query(api.user.GetUser, {
+        email: user?.email
+      })
+      setUser(result)
+    }
+  }
+
+  useEffect(() => {
+    isAuthenticated()
+  }, [])
+
+  
   return (
-    
+
     <div>
       <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
-        <UserContext.Provider value={{user,setUser}}>
-        <MessageContext.Provider value={{messages,setMessages}}>
-         <NextThemesProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          ></NextThemesProvider>
-      {children}
-      </MessageContext.Provider>
-      </UserContext.Provider>
+        <UserContext.Provider value={{ user, setUser }}>
+          <MessageContext.Provider value={{ messages, setMessages }}>
+            <NextThemesProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            ></NextThemesProvider>
+            {children}
+          </MessageContext.Provider>
+        </UserContext.Provider>
       </GoogleOAuthProvider>
     </div>
   )
