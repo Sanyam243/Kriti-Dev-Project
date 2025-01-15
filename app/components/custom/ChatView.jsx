5a4b5effbec63ddbdde9edf4c6cc7b9f54efc1ec
Sanyam243 <sanyamjain2240@gmail.com>
2025-01-15 22:29:28 +0530

    Gemini-Added

--- app/components/custom/ChatView.jsx
+++ app/components/custom/ChatView.jsx
@@ -1,11 +1,13 @@
 "use client"
-import { useConvex } from 'convex/react';
+import { useConvex, useMutation } from 'convex/react';
 import { useParams } from 'next/navigation'
 import React, { useContext, useEffect, useState } from 'react'
 import { api } from '../../../convex/_generated/api';
 import { UserContext } from '../../context/UserContext';
 import Image from 'next/image'
-import { Link, ArrowRight } from 'lucide-react'
+import { Link, ArrowRight, Loader2Icon } from 'lucide-react'
+import axios from 'axios';
+import Prompt from '../llm/Prompt';
 
 function ChatView() {
   const { workspaceId } = useParams();
@@ -13,7 +15,13 @@ function ChatView() {
   const [messages, setMessages] = useState()
   const { user, setUser } = useContext(UserContext)
   const [prompt, setPrompt] = useState("")
+  const [loading,setLoading] =useState(false)
+  const UpdateWorkspace = useMutation(api.workspace.UpdateWorkspace)
 
+  useEffect(() => {
+    workspaceId && GetWorkspace();
+
+  }, [workspaceId])
   const GetWorkspace = async () => {
     const result = await convex.query(api.workspace.GetWorkspace, {
       workspaceId: workspaceId
@@ -22,18 +30,61 @@ function ChatView() {
     setMessages(result?.messages)
   }
 
+  const GetAiResponse = async () => {
+    setLoading(true)
+    const response = await axios.post('/api/llm-chat', {
+      prompt: JSON.stringify(messages) + Prompt.CHAT_PROMPT
+    })
+
+    const AiResult = {
+      content: response.data.AiResponse,
+      role: 'ai'
+    }
+    setMessages((prev) => [...prev, AiResult])
+    setLoading(false)
+
+  }
+
+  const onGenerate = async (input) => {
+    const msg ={
+        role: 'user',
+        content: input
+    }
+    setMessages((prev) => [...prev, msg])
+
+    await UpdateWorkspace({
+      workspaceId:workspaceId,
+      messages:[...messages,msg]
+    })
+
+    setPrompt("")
+    
+}
+
+
+
   useEffect(() => {
-    GetWorkspace();
-  }, [])
+    if (messages?.length > 0) {
+      const role = messages[messages.length - 1].role
+      if (role == 'user') {
+        GetAiResponse();
+      }
+    }
+
+  }, [messages])
   return (
     <div className=' relative h-[85vh] flex flex-col p-2 gap-3 '>
-      <div className='flex-1 overflow-y-scroll'>
-        {messages && messages.map((message, index) => (
-          <div key={index} className='flex gap-2 bg-slate-600 p-2 rounded-md items-center '>
+      <div className='flex-1 gap-2 overflow-y-scroll p-3 scrollbar-hide '>
+        {messages && messages?.map((message, index) => (
+          <div key={index} className='flex mb-3 gap-2 bg-slate-600 p-2 rounded-md items-center leading-7 '>
+           
             {message.content}
-            {message?.role == 'user' && <Image className='rounded-full' src={user?.image} alt='User Image' width={40} height={40}></Image>}
+            {message?.role == 'user' && <Image className='rounded-full' src={user?.image||"image"} alt='User Image' width={40} height={40}></Image>}
+           
           </div>
+          
         ))}
+        {loading&& <div className='flex mb-3 gap-2 bg-slate-600 p-2 rounded-md items-center '> <Loader2Icon className='animate-spin'></Loader2Icon> Generating Response...</div>}
       </div>
 
 
