7f8c178be5f938dff9acb5b156d681b7418e5a44
Sanyam243 <sanyamjain2240@gmail.com>
2025-01-15 19:41:30 +0530

    Workspace-ui-chat-view added

--- app/components/custom/Header.jsx
+++ app/components/custom/Header.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+"use client"
+import React, { useContext } from 'react'
+import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 import { Button } from '../../../components/ui/button'
+import { UserContext } from '../../context/UserContext'
 
 function Header() {
+  const { user } = useContext(UserContext);
+  const router = useRouter();
+
+  const handleLogoClick = () => {
+    router.push('/');
+  };
+
   return (
-    <div className='flex p-3 items-center justify-between m-4'>
-      <Image src={'/logo.png'} alt='logo' width={40} height={40}></Image>
-      <div className='flex gap-3'>
-        <Button>Sign In</Button>
-        <Button>Get Started</Button>
-      </div>
+    <div className='flex p-2 items-center justify-between m-4'>
+      <Image 
+        onClick={handleLogoClick} 
+        src={'/logo.png'} 
+        alt='logo' 
+        width={40} 
+        height={40}
+        className="cursor-pointer" 
+      />
+      {!user?.name && (
+        <div className='flex gap-3'>
+          <Button>Sign In</Button>
+          <Button>Get Started</Button>
+        </div>
+      )}
     </div>
   )
 }
