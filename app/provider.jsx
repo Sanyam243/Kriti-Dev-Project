badc8be8d29dd7099e4d44345e332e6a0597cd34
Sanyam243 <sanyamjain2240@gmail.com>
2025-01-17 11:08:45 +0530

    Loaders added

--- app/provider.jsx
+++ app/provider.jsx
@@ -6,13 +6,20 @@ import { UserContext } from './context/UserContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { useConvex } from 'convex/react';
 import { api } from '../convex/_generated/api';
+import { useRouter } from 'next/navigation';
 function Provider({ children }) {
   const convex = useConvex();
+  const router = useRouter();
   const [messages, setMessages] = useState();
   const [user, setUser] = useState();
   const isAuthenticated = async () => {
     if (typeof window != undefined) {
       const user = JSON.parse(localStorage.getItem('user'))
+
+      if(!user){
+        router.push('/')
+        return;
+      }
       //Fetch the user
       const result = await convex.query(api.user.GetUser, {
         email: user?.email
