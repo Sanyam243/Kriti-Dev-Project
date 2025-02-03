696f94f624e13e6347bf066cfee74ffc3958bb62
Sanyam243 <sanyamjain2240@gmail.com>
2025-02-03 23:07:47 +0530

    Minor changes

--- app/components/custom/Footer.jsx
+++ app/components/custom/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           {/* Left Section - Branding & Socials */}
           <div className="mb-6 md:mb-0">
             <div className="flex items-center space-x-2">
-              <span className="text-2xl font-bold">💙 Developers</span>
+              <span className="text-2xl font-bold">💙 Manas Hostel</span>
             </div>
             {/* Social Icons */}
             <div className="flex space-x-4 mt-4">
@@ -67,7 +67,7 @@ const Footer = () => {
         <div className="mt-10 border-t border-gray-700 pt-4 text-center">
           <p className="text-gray-400 text-sm md:text-base font-medium">
             Crafted with ❤️ by 
-            <span className="text-white font-serif text-lg font-bold"> DEVELOPERS </span> 
+            <span className="text-white font-serif text-lg font-bold"> Manas Hostel </span> 
             
           </p>
         </div>
